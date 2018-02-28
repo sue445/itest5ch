deb2d140989568d9e89794abbe03b150f82c47bf
sue445 <sue445@sue445.net>
2018-03-01 01:41:14 +0900

    Remove dummy spec

--- spec/itest5ch_spec.rb
+++ spec/itest5ch_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Itest5ch do
   it "has a version number" do
     expect(Itest5ch::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
